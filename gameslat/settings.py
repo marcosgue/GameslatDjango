2e27090529e45b4d82e7eff3db8841145105632d
Marcos Guerrini <marcosguerrini@gmail.com>
2014-11-01 20:31:11 -0300

    Config

--- gameslat/settings.py
+++ gameslat/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'gameslat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'games_videojuegos',
+        'USER': 'root',
+        'PASSWORD': 'passparamac',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
