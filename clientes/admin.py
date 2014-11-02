53a1b6758110d1718f134ce46a1286f54edf6cd0
Marcos Guerrini <marcosguerrini@gmail.com>
2014-11-02 20:23:23 -0300

    Añadimos Clientes al Admin y definimos campos

--- clientes/admin.py
+++ clientes/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Cliente
 
-admin.site.register(Cliente)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'DNI_CUIT',
+        'phone',
+        'points'
+    )
